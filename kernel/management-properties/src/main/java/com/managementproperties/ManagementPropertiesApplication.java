78d86880e9657c23eb3ae9d5f3c2b0cfaea352de
bryan valdez <ibryan.valdez@gmail.com>
2019-06-20 22:57:03 -0500

    change

---
+++ kernel/management-properties/src/main/java/com/managementproperties/ManagementPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.managementproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementPropertiesApplication.class, args);
+	}
+
+}
